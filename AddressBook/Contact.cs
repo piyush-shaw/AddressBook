c41381cf5450487814f619a16f61b0b89b2ff816
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-13 18:42:49 +0530

    [Piyush] to add a new person contact to address book

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 namespace AddressBook
 {
-	public class Contact
-	{
-		public string FName { get; set; }
+    public class Contact
+    {
+        public string FName { get; set; }
         public string LName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        public long Zip { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
     }
 }
-
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,7 +4,29 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Address Book Program ");
         Contact contact = new Contact();
+        AddressBookMain addressBookMain = new AddressBookMain();
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Exit");
+            Console.WriteLine("\nEnter option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addressBookMain.AddContact();
+                    Console.WriteLine("\n");
+                    break;
+                case 2:
+                    addressBookMain.ViewDetail();
+                    Console.WriteLine("\n");
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
